d884db1950b249439cc6fb0e8af2f40846b2d66a
qudsalbreiki <qudsalbreiki@gmail.com>
2024-08-24 20:47:21 +0400

    Delete telusko/travello directory

--- telusko/travello/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import  path
-from . import views
-
-urlpatterns = [
-    path('',views.index, name ='index'), # / for home or you can keep it empty.
-]
-  #when someone calls for the home page we need to handle it with a function.
-    #in the home page we have a function called home.
-    #name ='home' or you can use index.html  
